4fb6b4ddaeeb83d0a80cd4fae8dbb05c622a0a5b
Rodrigo Sakae <devsakae@gmail.com>
2023-05-17 11:05:53 -0300

    fix: black estragou o test reading plan

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,6 +1,4 @@
-from tech_news.analyzer.reading_plan import (
-    ReadingPlanService,
-)  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
 
 
 def test_reading_plan_group_news():
